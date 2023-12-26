c12dfd3363622cfb0fe23d54215239b6e4475c23
Denys Lins <denyslins@gmail.com>
2023-12-26 15:51:43 -0300

    Add shopping cart

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { PRODUCT_CATEGORIES } from '@/config'
-import { useRef, useState } from 'react'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
+import { useEffect, useRef, useState } from 'react'
 import NavItem from './NavItem'
 
 const NavItems = () => {
@@ -10,8 +11,21 @@ const NavItems = () => {
 
   const navRef = useRef<HTMLDivElement | null>(null)
 
+  useOnClickOutside(navRef, () => setActiveIndex(null))
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+    document.addEventListener('keydown', handler)
+
+    return () => document.removeEventListener('keydown', handler)
+  }, [])
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const handleOpen = () => {
           if (activeIndex === index) {
